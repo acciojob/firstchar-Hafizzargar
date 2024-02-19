e80738a7cc2b8c9a8f5a3fe7cab5943caedb5ca0
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-02-19 11:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstChar(text) {
   // your code here
-	let z=text.trim();
+	let z=text.trim(); 
 	return z.charAt(0);
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const text = prompt("Enter text:");
-alert(firstChar(text);
+alert(firstChar(text));
