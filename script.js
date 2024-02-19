773318b7c80a8aa9112d8332bd7fe3be7e253b99
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-02-19 11:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 function firstChar(text) {
   // your code here
-	let z="";
-	let n=text.length;
-	if(n==0 || text[0]=="" ||text[0]==" "){
-		return "";
-	}
-	for(int i=0;i<n;i++){
-		if(text[i]!=" "){
-			z=z+text[i];
-		}
-	}
-	return z[0];
+	let z=text.trim();
+	return z.charAt(0);
 }
 
 // Do not change the code below
